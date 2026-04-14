9c7645e305cc70525e8d42389087a4e8c60bc27e
lalicon05 <lasse.l535@gmail.com>
2026-04-14 02:34:31 +0200

    started the wgpu tutorial and am temporarily doing it in state.rs

--- cargo/src/state.rs
+++ cargo/src/state.rs
@@ -1,1 +1,12 @@
 // This file will act as a core for the renderer
+
+
+// Currenlty this is just me following the tutorial and will change in the future.
+use std::sync::Arc;
+
+use winit::{application::ApplicationHandler, event::*, event_loop::{ActiveEventLoop, EventLoop}, keyboard::{KeyCode, PhysicalKey}, window::{Window}};
+
+struct State {
+	window: Arc<Window>,
+}
+
